a5dd22f59434e353b94629241441107d45dee60a
codingavinash <vishwakarmaavinash299@gmail.com>
2024-02-09 08:48:30 +0530

    Added copy button

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
-import { useState, useCallback , useEffect} from 'react'
+import { useState, useCallback , useEffect , useRef} from 'react'
 
 function App() {
   const [length, setLength] = useState(8);
   const [numberAllowed, setNumberAllowed] = useState(false);
   const [charAllowed, setCharAllowed] = useState(false);
   const [password, setPassword] = useState("");
+  // Ref hook
+  const passwordRef = useRef(null);
 
   const passwordGen = useCallback(()=>{
     let pass = "";
@@ -20,6 +22,12 @@ function App() {
     setPassword(pass);
   },[length, numberAllowed, charAllowed, setPassword]);
 
+  const copyPassword = useCallback(()=>{
+    passwordRef.current?.select();
+    passwordRef.current?.setSelectionRange(0,3);
+    window.navigator.clipboard.writeText(password);
+  },[password])
+
   useEffect(()=>{
     passwordGen();
   },[length, numberAllowed, charAllowed, passwordGen]);
@@ -29,13 +37,14 @@ function App() {
         <h1 className='text-white text-center my-3'>Password Generator</h1>
         <div className="flex shadow rounded-lg overflow-hidden mb-4">
           <input 
-          type="text" 
-          value={password}
-          className='outline-none w-full py-1 px-3'
-          placeholder='Password'
-          readOnly
+            type="text" 
+            value={password}
+            className='outline-none w-full py-1 px-3'
+            placeholder='Password'
+            readOnly
+            ref={passwordRef}
           />
-          <button className='outline-none bg-blue-700 text-white px-3 py-0.5 shrink-0'>Copy</button>
+          <button className='outline-none bg-blue-700 text-white px-3 py-0.5 shrink-0' onClick={copyPassword}>Copy</button>
         </div>
         <div className='flex text-sm gap-x-2'>
           <div className='flex items-center gap-x-1'>
